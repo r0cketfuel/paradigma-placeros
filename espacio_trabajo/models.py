08930c4f528d1ed69d6b33a2b8f424c0c87cc92c
eduardo s calvete <68311868+eduarsc@users.noreply.github.com>
2023-06-20 11:41:36 -0300

    add address to espacio_trabajo

--- espacio_trabajo/models.py
+++ espacio_trabajo/models.py
@@ -5,6 +5,7 @@ class EspacioTrabajo(models.Model):
     description = models.TextField(max_length=50)
     image = models.ImageField(
         upload_to='espacio_trabajo_images/', null=True, blank=True)
+    address = models.CharField(blank=True, max_length=30)
 
     def __str__(self):
         return self.description
