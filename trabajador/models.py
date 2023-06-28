a96213117cb0d9c68848bfa4e3f3ff0a58a2af18
eduardo s calvete <68311868+eduarsc@users.noreply.github.com>
2023-06-27 23:13:13 -0300

    add last name and dni to trabajador

--- trabajador/models.py
+++ trabajador/models.py
@@ -1,10 +1,11 @@
+import random
 from django.db import models
 
 
 class Trabajador(models.Model):
-    name = models.TextField()
+    name = models.TextField(default="")
+    last_name = models.TextField(default="")
+    dni = models.IntegerField()
 
     def __str__(self):
         return self.name
-# TODO
-# IMPLEMENT CLASSMETHOD FOR GET PK USERS
