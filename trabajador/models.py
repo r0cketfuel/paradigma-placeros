5b6719dc852a1ce891dcf2b8ddbc6e2e9d801af2
eduardo s calvete <68311868+eduarsc@users.noreply.github.com>
2023-06-27 23:24:36 -0300

    update

--- trabajador/models.py
+++ trabajador/models.py
@@ -1,11 +1,10 @@
-import random
 from django.db import models
 
 
 class Trabajador(models.Model):
-    name = models.TextField(default="")
-    last_name = models.TextField(default="")
-    dni = models.IntegerField()
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dni = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.name
