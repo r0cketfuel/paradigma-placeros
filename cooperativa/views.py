b7693266000f5aa062462738717f7efd86b03710
eduardo s calvete <68311868+eduarsc@users.noreply.github.com>
2023-07-12 06:03:17 -0300

    add numero de empleados por cooperativa

--- cooperativa/views.py
+++ cooperativa/views.py
@@ -1,10 +1,30 @@
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Cooperativa
 from .serializer import CooperativaSerializer
 from rest_framework.permissions import IsAuthenticated
+from planilla_trabajo.models import PlanillaTrabajo
 
 
 class CooperativaViewSet(viewsets.ModelViewSet):
     queryset = Cooperativa.objects.all()
     serializer_class = CooperativaSerializer
     permission_classes = [IsAuthenticated]
+
+
+class EmpleadosPorCooperativa(viewsets.ViewSet):
+
+    def list(self, request):
+        try:
+            planillas = PlanillaTrabajo.objects.all()
+            total_trabajadores_por_cooperativa = {}
+            for planilla in planillas:
+                cooperativa = planilla.id_plan_trabajo.id_cooperativa
+                print(cooperativa)
+                if cooperativa.description in total_trabajadores_por_cooperativa:
+                    total_trabajadores_por_cooperativa[cooperativa.description] += 1
+                else:
+                    total_trabajadores_por_cooperativa[cooperativa.description] = 1
+            return Response(total_trabajadores_por_cooperativa, status=200)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
