40395f396aa91f9c7d997eaac246ec5117bfb629
r0cketfuel <114667259+r0cketfuel@users.noreply.github.com>
2023-09-14 10:50:23 -0300

    Campos anidados en plan de trabajo

--- apps/plan_trabajo/serializer.py
+++ apps/plan_trabajo/serializer.py
@@ -1,37 +1,29 @@
 from rest_framework import serializers
-from .models import PlanTrabajo
-
+from .models        import PlanTrabajo
 
 class PlanTrabajoSerializer(serializers.ModelSerializer):
-    """
-    Serializador para la clase PlanTrabajo.
-
-    Este serializador se utiliza para convertir instancias de la clase PlanTrabajo
-    en representaciones JSON y viceversa. Se utiliza en la vista para realizar operaciones
-    CRUD (Crear, Leer, Actualizar, Eliminar) en los planes de trabajo.
-
-    Campos serializados:
-    - Todos los campos de la clase PlanTrabajo.
-
-    """
+    espacio = serializers.SerializerMethodField()
+    supervisor = serializers.SerializerMethodField()
+    cooperativa = serializers.SerializerMethodField()
 
     class Meta:
         model = PlanTrabajo
-        fields = '__all__'
+        fields = ('id', 'name', 'tipo_servicio', 'espacio', 'supervisor', 'cooperativa', 'fecha_creacion')
 
-    def to_representation(self, instance):
-
-        data = {
-            "id": instance.id,
-            "name": instance.name,
-            "tipo_servicio": instance.tipo_servicio,
-            "id_espacio": instance.id_espacio.id,
-            "espacio_name": instance.id_espacio.__str__(),
-            "id_supervisor": instance.id_supervisor.id,
-            "supervisor": instance.id_supervisor.__str__(),
-            "id_cooperativa": instance.id_cooperativa.id,
-            "cooperativa": instance.id_cooperativa.__str__(),
-            "creado": instance.fecha_creacion.strftime('%Y-%m-%d %H:%M:%S')
-
+    def get_espacio(self, obj):
+        return {
+            'id': obj.id_espacio.id,
+            'espacio_name': obj.id_espacio.description
         }
-        return data
+
+    def get_supervisor(self, obj):
+        return {
+            'id': obj.id_supervisor.id,
+            'supervisor': obj.id_supervisor.username
+        }
+
+    def get_cooperativa(self, obj):
+        return {
+            'id': obj.id_cooperativa.id,
+            'cooperativa': obj.id_cooperativa.description
+        }
--- apps/plan_trabajo/views.py
+++ apps/plan_trabajo/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import PlanTrabajo
 from .serializer import PlanTrabajoSerializer
 from apps.user_type.permisions import IsAdministrador, IsSuper
-
+from django.db.models import F
 
 class PlanTrabajoViewSet(viewsets.ModelViewSet):
     """
@@ -16,6 +16,12 @@ class PlanTrabajoViewSet(viewsets.ModelViewSet):
     - Todos los campos de la clase PlanTrabajo.
 
     """
-    queryset = PlanTrabajo.objects.all()
+    queryset = PlanTrabajo.objects.select_related(
+        'id_cooperativa', 'id_espacio', 'id_supervisor'
+    ).annotate(
+        cooperativa_name=F('id_cooperativa__description'),
+        espacio_nombre=F('id_espacio__description'),
+        supervisor_nombre=F('id_supervisor__username')
+    )
     serializer_class = PlanTrabajoSerializer
     permission_classes = [IsAdministrador | IsSuper]
