857d49181f924558418d43bd3c30a677d902703f
r0cketfuel <114667259+r0cketfuel@users.noreply.github.com>
2023-09-14 09:37:11 -0300

    Avances

--- apps/feriados/views.py
+++ apps/feriados/views.py
@@ -8,24 +8,21 @@ class FeriadoViewSet(viewsets.ModelViewSet):
     serializer_class = FeriadoSerializer
 
     def get_queryset(self):
-        try:
-            queryset = Feriado.objects.all()
-            year = self.request.query_params.get('ano')
-            month = self.request.query_params.get('mes')
-            day = self.request.query_params.get('dia')
+        queryset = Feriado.objects.all()
+        year = self.request.query_params.get('ano')
+        month = self.request.query_params.get('mes')
+        day = self.request.query_params.get('dia')
 
-            if year:
-                year = int(year)
-                queryset = queryset.filter(fecha__year=year)
-            
-            if month:
-                month = int(month)
-                queryset = queryset.filter(fecha__month=month)
-            
-            if day:
-                day = int(day)
-                queryset = queryset.filter(fecha__day=day)
+        if year:
+            year = int(year)
+            queryset = queryset.filter(fecha__year=year)
+        
+        if month:
+            month = int(month)
+            queryset = queryset.filter(fecha__month=month)
+        
+        if day:
+            day = int(day)
+            queryset = queryset.filter(fecha__day=day)
 
-            return queryset
-        except ValueError:
-            return Response({"error": "Uno o más parámetros no son números enteros válidos"}, status=status.HTTP_400_BAD_REQUEST)
+        return queryset
