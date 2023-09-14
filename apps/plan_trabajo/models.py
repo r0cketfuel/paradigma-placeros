80dc56acbb24b65008f8dea3e17f2ea462b46c1d
r0cketfuel <114667259+r0cketfuel@users.noreply.github.com>
2023-09-14 11:20:51 -0300

    Identación

--- apps/plan_trabajo/models.py
+++ apps/plan_trabajo/models.py
@@ -21,16 +21,12 @@ class PlanTrabajo(models.Model):
     - fecha_creacion: Fecha de creación del plan de trabajo.
 
     """
-    name = models.TextField()
-    id_cooperativa = models.ForeignKey(
-        Cooperativa, null=False, on_delete=models.CASCADE)  # type: ignore
-    id_espacio = models.ForeignKey(
-        EspacioTrabajo, null=False, on_delete=models.CASCADE)
-    id_supervisor = models.ForeignKey(
-        CustomUser, null=False, on_delete=models.CASCADE)
-    tipo_servicio = models.TextField()
-    fecha_creacion = models.DateTimeField(
-        auto_now_add=True, null=True)
+    name            = models.TextField()
+    id_cooperativa  = models.ForeignKey(Cooperativa,    null=False, on_delete=models.CASCADE)
+    id_espacio      = models.ForeignKey(EspacioTrabajo, null=False, on_delete=models.CASCADE)
+    id_supervisor   = models.ForeignKey(CustomUser,     null=False, on_delete=models.CASCADE)
+    tipo_servicio   = models.TextField()
+    fecha_creacion  = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
