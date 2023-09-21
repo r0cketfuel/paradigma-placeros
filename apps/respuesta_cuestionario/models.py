aaf5baef2e4a3faa6d6ade70c9bb64be852b0809
r0cketfuel <114667259+r0cketfuel@users.noreply.github.com>
2023-09-21 14:09:08 -0300

    Avances

--- apps/respuesta_cuestionario/models.py
+++ apps/respuesta_cuestionario/models.py
@@ -9,15 +9,8 @@ class RespuestaCuestionario(models.Model):
     Este modelo define los campos necesarios para representar las respuestas a un cuestionario,
     incluyendo la descripción, el cuestionario al que está asociado y una serie de respuestas
     booleanas para cada pregunta del cuestionario.
-
-    Campos del modelo:
-    - description: Descripción de la respuesta.
-    - id_cuestionario: Referencia al cuestionario al que pertenece la respuesta.
-    - answer1 a answer18: Respuestas booleanas a las preguntas del cuestionario.
-
     """
-    fecha               = models.DateTimeField(auto_now=True)
-    descripcion         = models.TextField()
+
     id_supervisor       = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, null=False, default=None)
     id_cuestionario     = models.ForeignKey(Cuestionario, on_delete=models.RESTRICT, null=False, default=None)
     respuesta1          = models.BooleanField(default=False)
@@ -38,9 +31,8 @@ class RespuestaCuestionario(models.Model):
     respuesta16         = models.BooleanField(default=False)
     respuesta17         = models.BooleanField(default=False)
     respuesta18         = models.BooleanField(default=False)
+    respuesta19         = models.BooleanField(default=False)
+    respuesta20         = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'respuestas_cuestionario'
-
-    def __str__(self):
-        return self.descripcion
