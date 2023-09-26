eaaf40b46d614edd1f18bf18b024597e5d5d63a9
r0cketfuel <114667259+r0cketfuel@users.noreply.github.com>
2023-09-26 10:11:46 -0300

    Delay

--- limpieza.sh
+++ limpieza.sh
@@ -25,3 +25,10 @@ cd docker
 docker-compose build
 docker-compose up -d
 cd ..
+
+sleep 5
+
+# Elimina todas las migraciones que coincidan con el patrón "nnnn_nombre.py"
+cd apps
+find . -name '????_*.py' -delete
+cd ..
