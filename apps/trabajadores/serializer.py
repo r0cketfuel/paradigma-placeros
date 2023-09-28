2f5850cbf117b55866b2d951da26a5ecb7742ee4
r0cketfuel <unleashedwings@gmail.com>
2023-09-28 02:22:13 +0000

    Campo nombre de cooperativa

--- apps/trabajadores/serializer.py
+++ apps/trabajadores/serializer.py
@@ -1,8 +1,17 @@
-from rest_framework import serializers
-from .models        import Trabajador
+from rest_framework             import serializers
+from .models                    import Trabajador
+from apps.cooperativas.models   import Cooperativa
+
+
+class CooperativaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model   = Cooperativa
+        fields  = ['nombre']
 
 
 class TrabajadorSerializer(serializers.ModelSerializer):
+
+    cooperativa_nombre = serializers.ReadOnlyField(source='id_cooperativa.nombre')
 
     class Meta:
         model   = Trabajador
