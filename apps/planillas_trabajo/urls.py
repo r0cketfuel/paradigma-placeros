ecf9093f50f6b9844030608704690e0d11a2d463
r0cketfuel <unleashedwings@gmail.com>
2023-10-02 09:07:07 -0300

    Limpieza código viejo

--- apps/planillas_trabajo/urls.py
+++ apps/planillas_trabajo/urls.py
@@ -1,12 +1,10 @@
 from django.urls                    import path, include
 from rest_framework.routers         import DefaultRouter
-from apps.planillas_trabajo.views   import PlanillaTrabajoViewSet, TrabajadoresInPlanillaTrabajoByIdPlanTrabajoViewSet, PresentesEntreFechasPorIdTrabajador
+from apps.planillas_trabajo.views   import PlanillaTrabajoViewSet
 
 
 router = DefaultRouter()
-router.register(r'planillas_trabajo',                           PlanillaTrabajoViewSet,                                 basename='planillas_trabajo')
-router.register(r'trabajadores_por_id_plantrabajo',             TrabajadoresInPlanillaTrabajoByIdPlanTrabajoViewSet,    basename='trabajadores_por_id_plantrabajo')
-#router.register(r'asistencias_entre_fechas_por_trabajador',    PresentesEntreFechasPorIdTrabajador,                    basename='asistencias_entre_fechas_por_trabajador')
+router.register(r'planillas_trabajo', PlanillaTrabajoViewSet, basename='planillas_trabajo')
 
 urlpatterns = [
     path('', include(router.urls)),
