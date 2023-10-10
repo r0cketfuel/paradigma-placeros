eadd34f279e681301dcbd6529734c7af71269cd2
r0cketfuel <unleashedwings@gmail.com>
2023-10-10 08:48:28 -0300

    Fix ingreso de datos

--- apps/planes_trabajo/models.py
+++ apps/planes_trabajo/models.py
@@ -5,22 +5,7 @@ from apps.users.models              import CustomUser
 
 
 class PlanTrabajo(models.Model):
-    """
-    Modelo para representar un plan de trabajo.
 
-    Este modelo define los campos necesarios para representar un plan de trabajo, incluyendo
-    su nombre, la cooperativa asociada, el espacio de trabajo asignado, el supervisor
-    responsable, el tipo de servicio y la fecha de creación.
-
-    Campos del modelo:
-    - name: Nombre del plan de trabajo.
-    - cooperativa: Referencia a la cooperativa asociada.
-    - espacio: Referencia al espacio de trabajo asignado.
-    - supervisor: Referencia al supervisor responsable.
-    - tipo_servicio: Tipo de servicio del plan de trabajo.
-    - fecha_creacion: Fecha de creación del plan de trabajo.
-
-    """
     nombre          = models.TextField()
     tipo_servicio   = models.TextField()
     cooperativa     = models.ForeignKey(Cooperativa, null=False, on_delete=models.RESTRICT)
--- apps/planes_trabajo/serializer.py
+++ apps/planes_trabajo/serializer.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from .models        import PlanTrabajo
 
+from apps.espacios_trabajo.models import EspacioTrabajo
+from apps.users.models import CustomUser
+from apps.cooperativas.models import Cooperativa
 
 class PlanTrabajoSerializer(serializers.ModelSerializer):
     
     nombre          = serializers.CharField()
     tipo_servicio   = serializers.CharField()
+    espacio_id      = serializers.PrimaryKeyRelatedField(queryset=EspacioTrabajo.objects.all(), source='espacio', write_only=True)
+    supervisor_id   = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), source='supervisor', write_only=True)
+    cooperativa_id  = serializers.PrimaryKeyRelatedField(queryset=Cooperativa.objects.all(), source='cooperativa', write_only=True)
+
     espacio         = serializers.SerializerMethodField()
     supervisor      = serializers.SerializerMethodField()
     cooperativa     = serializers.SerializerMethodField()
@@ -17,7 +24,7 @@ class PlanTrabajoSerializer(serializers.ModelSerializer):
     def get_espacio(self, obj):
         return {
             'id':           obj.espacio.id,
-            'espacio_name': obj.espacio.nombre
+            'nombre':       obj.espacio.nombre
         }
 
     def get_supervisor(self, obj):
