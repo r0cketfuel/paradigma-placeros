902c4abb36e25367dcd122283763128f16233182
r0cketfuel <unleashedwings@gmail.com>
2023-11-28 18:28:55 -0300

    Trabajadores por ID plan de trabajo

--- apps/planillas_trabajo/urls.py
+++ apps/planillas_trabajo/urls.py
@@ -1,10 +1,11 @@
 from django.urls                    import path, include
 from rest_framework.routers         import DefaultRouter
-from apps.planillas_trabajo.views   import PlanillaTrabajoViewSet
+from apps.planillas_trabajo.views   import PlanillaTrabajoViewSet, TrabajadoresInPlanillaTrabajoByIdPlanTrabajoViewSet
 
 
 router = DefaultRouter()
-router.register(r'planillas_trabajo', PlanillaTrabajoViewSet, basename='planillas_trabajo')
+router.register(r'planillas_trabajo',               PlanillaTrabajoViewSet,                                 basename='planillas_trabajo')
+router.register(r'trabajadores_por_id_plantrabajo', TrabajadoresInPlanillaTrabajoByIdPlanTrabajoViewSet,    basename='trabajadores_por_id_plantrabajo')
 
 urlpatterns = [
     path('', include(router.urls)),
--- apps/planillas_trabajo/views.py
+++ apps/planillas_trabajo/views.py
@@ -1,6 +1,9 @@
 from rest_framework                 import viewsets, status
+from rest_framework.response        import Response
 from .models                        import PlanillaTrabajo
+from apps.trabajadores.models       import Trabajador
 from .serializer                    import PlanillaTrabajoSerializer
+from apps.trabajadores.serializer   import TrabajadorSerializer
 from apps.user_type.permisions      import IsAdministrador, IsSuper, IsSupervisor
 
 
@@ -15,3 +18,48 @@ class PlanillaTrabajoViewSet(viewsets.ModelViewSet):
     queryset            = PlanillaTrabajo.objects.all()
     serializer_class    = PlanillaTrabajoSerializer
     permission_classes  = [IsAdministrador | IsSuper]
+
+class TrabajadoresInPlanillaTrabajoByIdPlanTrabajoViewSet(viewsets.ViewSet):
+    """
+    Vista para obtener la lista de trabajadores en una planilla de trabajo por ID de plan de trabajo.
+
+    Esta vista permite obtener la lista de trabajadores que están asignados a una planilla de
+    trabajo específica identificada por su ID de plan de trabajo. Se espera que se proporcione
+    el ID de plan de trabajo en el cuerpo de la solicitud POST.
+
+    Parámetros de la solicitud:
+    - plan_trabajo_id: ID del plan de trabajo para el cual se desea obtener la lista de trabajadores.
+
+    Ejemplo de solicitud JSON:
+{
+    "plan_trabajo_id": 1
+}
+
+    La vista realiza una consulta en la base de datos y devuelve la lista de trabajadores
+    asignados a la planilla de trabajo especificada.
+
+    Permisos requeridos:
+    - El usuario debe tener permisos de administrador, superusuario o supervisor
+      para acceder a esta vista.
+    """
+
+    permission_classes = [IsAdministrador | IsSuper | IsSupervisor]
+
+    def create(self, request):
+        try:
+            if request.data.get("plan_trabajo_id", None):
+                id = request.data.get('plan_trabajo_id')
+                planilla_today = PlanillaTrabajo.objects.filter(
+                    plan_trabajo_id=id)
+                if planilla_today:
+                    trabajadores = []
+                    for planilla in planilla_today:
+                        trabajadores.append(Trabajador.objects.filter(
+                            id=planilla.trabajador_id).first())   # type: ignore
+                    serializer = TrabajadorSerializer(
+                        trabajadores, many=True)
+                    return Response(serializer.data)
+                return Response([])
+            return Response({"error": "no se reconoce el identificador"})
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
